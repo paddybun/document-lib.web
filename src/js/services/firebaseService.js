2ab5bd0d2023de9cd5ed4c4711f87636da0ebf24
pbu <1834214+paddybun@users.noreply.github.com>
2018-08-15 14:28:00 +0200

    MASTER  - Remove api information from firebaseService :( (also deleted firebase  project)

--- src/js/services/firebaseService.js
+++ src/js/services/firebaseService.js
@@ -1,17 +1,6 @@
 import firebase from 'firebase'
 require('firebase/firestore')
 
-debugger
-
-// var config = {
-//     apiKey: "AIzaSyAhPsA2DVrhmjGlODenbNTwe_1o5qziYvc",
-//     authDomain: "document-lib.firebaseapp.com",
-//     databaseURL: "https://document-lib.firebaseio.com",
-//     projectId: "document-lib",
-//     storageBucket: "document-lib.appspot.com",
-//     messagingSenderId: "1001851846450"
-// };
-
 firebase.initializeApp(config)
 const firestore = firebase.firestore()
 const settings = { timestampsInSnapshots: true }
