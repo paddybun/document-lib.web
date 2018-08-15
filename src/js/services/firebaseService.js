8a1b0f85e9c6d41363cc37b3d6d2970e074187b7
pbu <1834214+paddybun@users.noreply.github.com>
2018-08-15 14:09:06 +0200

    MASTER  - Update webpack config to support dotenv

--- src/js/services/firebaseService.js
+++ src/js/services/firebaseService.js
@@ -1,9 +1,16 @@
 import firebase from 'firebase'
 require('firebase/firestore')
 
-var config = {
+debugger
 
-};
+// var config = {
+//     apiKey: "AIzaSyAhPsA2DVrhmjGlODenbNTwe_1o5qziYvc",
+//     authDomain: "document-lib.firebaseapp.com",
+//     databaseURL: "https://document-lib.firebaseio.com",
+//     projectId: "document-lib",
+//     storageBucket: "document-lib.appspot.com",
+//     messagingSenderId: "1001851846450"
+// };
 
 firebase.initializeApp(config)
 const firestore = firebase.firestore()
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,34 @@
 const debug = process.env.NODE_ENV !== 'production'
-const dotenv = require('dotenv');
+const Dotenv = require('dotenv-webpack');
 const path = require('path')
 const webpack = require('webpack')
 
-
-module.exports = {
-  devtool: debug ? 'inline-sourcemap' : null,
-  entry: ['whatwg-fetch', './src/js/client.js'],
-  module: {
-    loaders: [
-      {
-        test: /\.js?$/,
-        exclude: '/(node_modules|bower_components)/',
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-0'],
-          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
+module.exports = () => {
+  return {
+    devtool: debug ? 'inline-sourcemap' : null,
+    entry: ['whatwg-fetch', './src/js/client.js'],
+    module: {
+      loaders: [
+        {
+          test: /\.js?$/,
+          exclude: '/(node_modules|bower_components)/',
+          loader: 'babel-loader',
+          query: {
+            presets: ['react', 'env', 'stage-0'],
+            plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
+          }
         }
-      }
+      ]
+    },
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'build.js'
+    },
+    plugins: debug ? [] : [
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.OccurenceOrderPlugin(),
+      new webpack.optimize.UglifyJsPlugin({mangle: false, sourcemap: false}),
+      new Dotenv()
     ]
-  },
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'build.js'
-  },
-  plugins: debug ? [] : [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({mangle: false, sourcemap: false})
-  ]
+  }
 };
